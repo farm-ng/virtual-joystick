6d544a07b1505cabe2de0f697498178b46c5a30d
Kyle Coble <kwc57@cornell.edu>
2023-01-29 12:29:44 -0300

    VirtualJoystickWidget as importable widget

--- src/main.py
+++ src/main.py
@@ -14,11 +14,9 @@
 import argparse
 import asyncio
 import os
-from math import sqrt
 from typing import Generator
 from typing import List
 from typing import Optional
-from typing import Tuple
 
 import grpc
 from farm_ng.canbus import canbus_pb2
@@ -32,7 +30,7 @@ from farm_ng.oak.camera_client import OakCameraClient
 from farm_ng.service import service_pb2
 from farm_ng.service.service_client import ClientConfig
 from turbojpeg import TurboJPEG
-
+from virtual_joystick.joystick import VirtualJoystickWidget
 
 # Must come before kivy imports
 os.environ["KIVY_NO_ARGS"] = "1"
@@ -49,99 +47,8 @@ Config.set("kivy", "keyboard_mode", "systemanddock")
 from kivy.app import App  # noqa: E402
 from kivy.clock import Clock  # noqa: E402
 from kivy.graphics.texture import Texture  # noqa: E402
-from kivy.input.providers.mouse import MouseMotionEvent  # noqa: E402
 from kivy.lang.builder import Builder  # noqa: E402
 from kivy.properties import StringProperty  # noqa: E402
-from kivy.uix.widget import Widget  # noqa: E402
-
-
-class Vec2:
-    """Simple container for keeping joystick coords in x & y terms.
-
-    Defaults to a centered joystick (0,0). Clips values to range [-1.0, 1.0], as with the Amiga joystick.
-    """
-
-    def __init__(self, x: float = 0.0, y: float = 0.0) -> None:
-        self.x: float = min(max(-1.0, x), 1.0)
-        self.y: float = min(max(-1.0, y), 1.0)
-
-    def __str__(self) -> str:
-        return f"({self.x:0.2f}, {self.y:0.2f})"
-
-
-class VirtualJoystickWidget(Widget):
-    def __init__(self, **kwargs) -> None:
-        super(VirtualJoystickWidget, self).__init__(**kwargs)
-
-        self.joystick_pose: Vec2 = Vec2()
-
-        # Schedule the drawing of the joystick at 30 hz
-        Clock.schedule_interval(self.draw_joystick, 1 / 30)
-
-    def on_touch_down(self, touch: MouseMotionEvent) -> None:
-        """Overwrite kivy method that handles initial press with mouse click or touchscreen.
-
-        NOTE: This is called regardless of whether this is the touched widget.
-        """
-        # Check if touch is in this widget using kivy ``collide_point`` method
-        if not self.collide_point(*touch.pos):
-            return
-
-        self.update_joystick_pose(touch)
-
-    def on_touch_move(self, touch: MouseMotionEvent) -> None:
-        """Overwrite kivy method that handles when press is held and dragged with mouse click or touchscreen.
-
-        NOTE: This is called regardless of whether this is the touched widget.
-        """
-        # Check if touch is in this widget using kivy ``collide_point`` method
-        if not self.collide_point(*touch.pos):
-            return
-
-        self.update_joystick_pose(touch)
-
-    def on_touch_up(self, touch: MouseMotionEvent) -> None:
-        """Overwrite kivy method that handles release of press with mouse click or touchscreen.
-
-        NOTE: This is called regardless of whether this is the touched widget.
-        """
-        # Reset joystick pose, regardless of where touch_up occurs
-        self.joystick_pose: Vec2 = Vec2()
-
-    def update_joystick_pose(self, touch: MouseMotionEvent) -> None:
-        assert self.collide_point(*touch.pos), "Only pass touches "
-
-        res: Tuple[float, float] = self.relative_cord_in_widget(touch)
-
-        # Clip to unit circle
-        div: float = max(1.0, sqrt(res[0] ** 2 + res[1] ** 2))
-        self.joystick_pose = Vec2(x=res[0] / div, y=res[1] / div)
-
-    def relative_cord_in_widget(self, touch: MouseMotionEvent) -> Tuple[float, float]:
-        """Returns the coordinates of the touch on the scale IFF it occurs within the bounds of the widget."""
-
-        # Range to put the values on
-        scale: Tuple[float, float] = (-1.0, 1.0)
-
-        # Map coord onto scale range
-        return (
-            scale[0] + (touch.x - self.pos[0]) * (scale[1] - scale[0]) / (self.width),
-            scale[0] + (touch.y - self.pos[1]) * (scale[1] - scale[0]) / (self.height),
-        )
-
-    def draw_joystick(self, dt: float = 0.0):
-        """Update the drawn pose of the joystick, in pixel coords."""
-        self.joystick_position_x = (
-            self.center_x
-            + 0.5 * self.joystick_pose.x * (self.width - self.joystick_diameter)
-            - self.joystick_diameter // 2
-        )
-
-        self.joystick_position_y = (
-            self.center_y
-            + 0.5 * self.joystick_pose.y * (self.height - self.joystick_diameter)
-            - self.joystick_diameter // 2
-        )
 
 
 class VirtualJoystickApp(App):
